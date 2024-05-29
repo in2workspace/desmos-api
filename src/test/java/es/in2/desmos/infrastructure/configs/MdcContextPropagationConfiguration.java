9ba5c5fb5c9f78928f122d807134d8d0c6cc43a8
Albert Rodríguez <albert.rodriguez@in2.es>
2024-05-29 08:18:05 +0200

    chore(project): add context propagation

---
+++ src/test/java/es/in2/desmos/infrastructure/configs/MdcContextPropagationConfiguration.java
@@ -0,0 +1,41 @@
+package es.in2.desmos.infrastructure.configs;
+
+import io.micrometer.context.ContextRegistry;
+import io.micrometer.context.ContextSnapshotFactory;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Hooks;
+
+import java.util.List;
+
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+
+/**
+ * 1. Will register ThreadLocalAccessors into ContextRegistry for fields listed in application.yml as property value
+ * <b>management.tracing.baggage.correlation.fields</b>
+ * 2. Enables Automatic Context Propagation for all reactive methods
+ *
+ * @see <a href="https://github.com/micrometer-metrics/context-propagation">context-propagation</a>
+ */
+@Configuration
+@ConditionalOnClass({ContextRegistry.class, ContextSnapshotFactory.class})
+@ConditionalOnProperty(value = "management.tracing.baggage.correlation.fields", matchIfMissing = true)
+public class MdcContextPropagationConfiguration {
+
+    public MdcContextPropagationConfiguration(@Value("${management.tracing.baggage.correlation.fields}")
+                                              List<String> fields) {
+        if (!isEmpty(fields)) {
+            fields.forEach(claim -> ContextRegistry.getInstance()
+                    .registerThreadLocalAccessor(claim,
+                            () -> MDC.get(claim),
+                            value -> MDC.put(claim, value),
+                            () -> MDC.remove(claim)));
+            return;
+        }
+
+        Hooks.enableAutomaticContextPropagation();
+    }
+}
